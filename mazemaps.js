8de9ced4e241f69c2f7a2293ecbc5a073e07ed2a
eSol-Michelle-Chen <Michelle.Chen@monash.edu>
2022-09-20 09:00:02 +1000

    Open in new window

--- mazemaps.js
+++ mazemaps.js
@@ -5,7 +5,7 @@
 var map = new Mazemap.Map({
   // container id specified in the HTML
   container: "map",
-  campuses: "default",
+  campuses: 159,
   // initial position in lngLat format
   center: { lng: 145.1327, lat: -37.9131 }, // longitude & latitude coordinates of Monash University Clayton Campus
 
@@ -23,8 +23,20 @@ map.addControl(new Mazemap.mapboxgl.NavigationControl());
 map.on("load", function () {
   // MazeMap ready
 
-  var spear_coordinates = { lng: 145.13433289910176, lat: -37.91301299847796 };
-  createMarkers(map, spear_coordinates);
+  var spearCoordinates = { lng: 145.13433289910176, lat: -37.91301299847796 };
+  var firepitCoordinates = {
+    lng: 145.13278060280857,
+    lat: -37.913076644295764,
+  };
+
+  var locationsList = [spearCoordinates, firepitCoordinates];
+
+  // For all locations in locationsList, create a marker
+  for (let i = 0; i < locationsList.length; i++) {
+    var coordinates = locationsList[i];
+    createMarkers(map, coordinates);
+  }
+  // createMarkers(map, spear_coordinates);
   map.on("click", onMapClick);
 });
 
@@ -59,7 +71,7 @@ function createMarkers(map, lngLat) {
       closeOnClick: true,
       offset: [0, -27],
     }).setHTML(
-      '<h3>AR experience</h3><p>Explanation of the indegenous location point </p><p style="max-width: 200px;"> Click the <a href = "https://esol-michelle-chen.github.io/MonkeyPox/" target> link</a> and scan the AR marker to view an AR experience</p>'
+      '<h3>AR experience</h3><p>Explanation of the indegenous location point </p><p style="max-width: 200px;"> Click the <a href = "https://esol-michelle-chen.github.io/MonkeyPox/" target="_blank"> link</a> and scan the AR marker to view an AR experience</p>'
     );
 
     marker.setPopup(popup);
@@ -75,5 +87,6 @@ function onMapClick(e) {
   // var longitude = e.longitude;
   var lngLat = e.lngLat;
   console.log(lngLat);
+  console.log(Mazemap.Data.getPoiAt(lngLat, 1));
   createMarkers(map, lngLat);
 }
